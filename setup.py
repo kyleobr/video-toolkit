0b00cfcd4a826af074620cc6ced419d2bcb98939
kyleobr <coolrf014@hotmail.com>
2026-05-09 11:33:47 +0800

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="video-toolkit",
+    version="0.2.1",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    description="Lightweight video metadata extraction and thumbnail generation",
+    author="kyleobr",
+    license="MIT",
+)
