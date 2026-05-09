7e86ba9e983c66855a235434ca4681d3868cfb51
kyleobr <coolrf014@hotmail.com>
2026-05-09 11:39:16 +0800

    feat: init package

--- video_toolkit/__init__.py
+++ video_toolkit/__init__.py
@@ -1,4 +1,4 @@
-"""video-toolkit: lightweight video metadata extraction"""
+"""video-toolkit"""
 __version__ = "0.2.1"
 from .extractor import extract_metadata
 from .thumbnail import generate_thumbnail
